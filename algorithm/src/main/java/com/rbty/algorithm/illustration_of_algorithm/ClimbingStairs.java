39f4568a6e106a4a638dff1714bb6e258f1e1994
RabbitYong <tufu432922@qq.com>
2021-03-22 13:25:50 +0800

    11

---
+++ algorithm/src/main/java/com/rbty/algorithm/illustration_of_algorithm/ClimbingStairs.java
@@ -0,0 +1,17 @@
+package com.rbty.algorithm.illustration_of_algorithm;
+
+public class ClimbingStairs {
+    public static int numWays(int n) {
+        int a=1,b=1,sum;
+        for (int i = 0; i < n; i++) {
+            sum = a+b;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numWays(0));
+    }
+}
